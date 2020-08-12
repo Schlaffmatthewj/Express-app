759e1d92a24ddd8658015d14902a199503e7414e
Matthew Schlaff <schmayley@Matthews-Air.attlocal.net>
2020-08-11 18:17:41 -0600

    server.js is nearly complete, minus the views

--- server.js
+++ server.js
@@ -1,15 +1,33 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const passport = require('passport');
 
+const authRouter = require('./routes/auth-router');
+const searchRouter = require('./routes/search-router');
+const userRouter = require('./routes/user-router');
 
 const app = express();
 require('dotenv').config();
 
+app.use.methodOverride('_method');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({
+    secret: process.env.SECRET_KEY,
+    resave: true,
+    saveUninitialized: true,
+}));
+app.use(passport.initialize());
+app.use(passport.initialize.session());
 
+
+app.set('views', 'views');
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 const PORT = process.env.PORT || 3000;
@@ -19,11 +37,21 @@ app.listen(PORT, () => {
 
 app.get('/', (req, res) => {
     res.send('HELLO WORLD');
+    // CHANGE TO THE PROPER INDEX VIEW WHEN AVAILABLE
 });
+
+
+app.use('/auth', authRouter);
+app.use('/search', searchRouter);
+app.use('/user', userRouter);
 
 
 app.use('*', (req, res) => {
     res.status(404).send({
         error: 'Not Found',
     });
+});
+
+app.use((err, req, res, next) => {
+    res.status(500).send({ err, message: err.message });
 });
